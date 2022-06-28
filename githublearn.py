9eaf5b807c008164a9d4be3fbc0021fb3e5c207d
lcarasik <lcar722@gmail.com>
2022-06-28 12:25:00 -0400

    Adding changes from .py file including making a coarse parabolic profile

--- githublearn.py
+++ githublearn.py
@@ -14,7 +14,9 @@ import pandas as pd
 # Define variables
 
 x = np.linspace(0,4,30)
+x1 = np.linspace(0,4,5)
 y = x**2
+y1 = x1**2
 
 #--------------------------------------
 # Plot
@@ -22,6 +24,8 @@ y = x**2
 k = 1
 plt.figure(k)
 plt.plot(x,y)
+plt.plot(x1,y1)
+
 
 k = k + 1
 plt.figure(k)
